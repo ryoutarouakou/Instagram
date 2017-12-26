9d42d2314e47331ea3dfd4f964f2d77fa990d4c4
ryoutarouakou <allergy.blue@gmail.com>
2017-12-26 14:14:24 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 赤穂亮太郎 on 2017/12/26.
+//  Copyright © 2017年 ryoutarou.akou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
