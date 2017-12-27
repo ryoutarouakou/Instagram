b774b6d00e8b372e0afce97780e1d81a3a54ec6b
ryoutarouakou <allergy.blue@gmail.com>
2017-12-27 14:22:26 +0900

    Lesson8 5.7ログイン画面まで

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import ESTabBarController
+import Firebase // 先頭でFirebaseをimportしておく
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -19,6 +21,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // currentUserがnilならログインしていない
+        if Auth.auth().currentUser == nil {
+            // ログインしていないときの処理
+            let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+            self.present(loginViewController!, animated: true, completion: nil)
+        }
     }
     
     func setupTab() {
