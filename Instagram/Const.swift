4d2874f4f78b250c972099415995efdab35b7476
ryoutarouakou <allergy.blue@gmail.com>
2017-12-28 16:14:08 +0900

    8.8.3終了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 赤穂亮太郎 on 2017/12/28.
+//  Copyright © 2017年 ryoutarou.akou. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
